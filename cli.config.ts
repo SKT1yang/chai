208769d5c196293866eff7ac20edec1c2c415b3a
SKT1yang <34095649+SKT1yang@users.noreply.github.com>
2026-03-19 17:12:59 +0800

    chore: 更新 .gitignore 和配置文件
    - 在 .gitignore 中添加 *.tsbuildinfo
    - 更新 .vscode/settings.json 和 .vscode/extensions.json 的格式
    - 在 cli.config.ts 中添加 *.tsbuildinfo 和 .idea 到默认排除列表
    - 在 package.json 中添加 postinstall 脚本，全局安装 @shuiyangsuan/cli

--- cli.config.ts
+++ cli.config.ts
@@ -1,7 +1,15 @@
 import type { CliConfig } from '@shuiyangsuan/cli'
 
 const cliConfig: CliConfig = {
-	defaultPatterns: ['**/node_modules', '**/dist', '**/.turbo', '**/*.vsix', '**/.DS_Store'],
+	defaultPatterns: [
+		'**/node_modules',
+		'**/dist',
+		'**/.turbo',
+		'**/*.vsix',
+		'**/.DS_Store',
+		'**/*.tsbuildinfo',
+		'**/.idea',
+	],
 	verbose: true,
 }
 
